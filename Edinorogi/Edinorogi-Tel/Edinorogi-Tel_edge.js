792f30131ce3a045d9974fbda8e80cb984d4c91a
Asqwerty <Asqwerty04@mail.ru>
2017-11-01 01:47:18 +0500

    1

---
+++ Edinorogi/Edinorogi-Tel/Edinorogi-Tel_edge.js
@@ -0,0 +1,7 @@
+
+(function(compId){var _=null,y=true,n=false,i='none',x6='rgba(255,255,255,0.00)',x2='5.0.0',x4='horizontal',x3='5.0.1.386',x1='5.0.1';var g5='%D0%A8%D1%82%D0%BE%D1%80%D1%8B.mp4';var im='images/',aud='media/',vid='media/',js='js/',fonts={},opts={'gAudioPreloadPreference':'auto','gVideoPreloadPreference':'auto'},resources=[],scripts=[],symbols={"stage":{v:x1,mv:x2,b:x3,stf:i,cg:x4,rI:n,cn:{dom:[{id:'Media',t:'video',tag:'video',r:['0px','0px','100%','72px','auto','auto'],overflow:'hidden',tt:'Edinorogi',ap:'autoplay',sr:[vid+g5],pr:'auto',ps:'images/Poster.png'}],style:{'${Stage}':{isStage:true,r:['null','null','100%','72px','auto','auto'],zr:['0px','','',''],overflow:'hidden',f:[x6]}}},tt:{d:8591,a:y,data:[["eid1","tr",0,function(e,d){this.eMA(e,d);},['play','${Media}',[]]],["eid2","tr",8591,function(e,d){this.eMA(e,d);},['pause','${Media}',[]]]]}}};AdobeEdge.registerCompositionDefn(compId,symbols,fonts,scripts,resources,opts);})("EDGE-14543493");
+(function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;Edge.registerEventBinding(compId,function($){
+//Edge symbol: 'stage'
+(function(symbolName){})("stage");
+//Edge symbol end:'stage'
+})})(AdobeEdge.$,AdobeEdge,"EDGE-14543493");
